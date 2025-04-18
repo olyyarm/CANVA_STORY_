44c1bf7900c5315d8984cbdcb9ebe42d20863ce9
olyyarm <126116462+olyyarm@users.noreply.github.com>
2025-04-18 13:49:08 +0300

    Configure base path in vite.config.ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CANVA_STORY_/', // Устанавливаем базовый путь для GitHub Pages
 })

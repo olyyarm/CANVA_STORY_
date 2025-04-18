e8fc6f08a46bca07fe40b8f459f2352806facb75
olyyarm <126116462+olyyarm@users.noreply.github.com>
2025-04-18 15:05:28 +0300

    Prepend BASE_URL to icon paths in getNodeIcon

--- src/components/NodeRenderer.tsx
+++ src/components/NodeRenderer.tsx
@@ -135,7 +135,7 @@ return (
                                 title="Копировать текст"
                                 onMouseDown={stopPropagation}
                             >
-                                <img src="/icon/copy.svg" alt="Copy" className="w-3 h-3" />
+                                <img src="/copy.svg" alt="Copy" className="w-3 h-3" />
                             </button>
                         )}
                     </div>
@@ -194,7 +194,7 @@ return (
                                         className="copy-to-clipboard-button absolute top-1 right-1 w-5 h-5 p-0 border-none bg-black bg-opacity-50 rounded cursor-pointer flex items-center justify-center"
                                         title="Копировать текст"
                                         onMouseDown={stopPropagation} >
-                                        <img src="/icon/copy.svg" alt="Copy" className="w-3 h-3" />
+                                        <img src="/copy.svg" alt="Copy" className="w-3 h-3" />
                                     </button>
                                 </div>
                             )}
